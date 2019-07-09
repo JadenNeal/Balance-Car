65427cafd43c79303841123f4c90c0b8318de600
Jaden Neal <longweicai1997@163.com>
2019-07-09 12:55:04 +0800

    Bluetooth

--- Bluetooth/Bluetooth.ino
+++ Bluetooth/Bluetooth.ino
@@ -1,24 +1,27 @@
-#include <SoftwareSerial.h>
-//使用软件串口，能将数字口模拟成串口
-SoftwareSerial BT(2, 3);  //新建对象，板子的接收脚为2，发送脚为3
+//#include <I2Cdev.h>
+/*
+连线方法：
+RX -- TX
+TX -- RX
+VCC -- 5V
+GND -- GND
+下载程序时，先将VCC断开，然后上传程序，接着再接上VCC，
+用手机连接蓝牙即可。
+*/
+
 char val;  //存储接收的变量
 
 void setup() {
   Serial.begin(9600);   //与电脑的串口连接
   Serial.println("Hc-06 is ready!");
-  BT.begin(9600);  //设置波特率
+//  BT.begin(9600);  //设置波特率
 }
 
 void loop() {
-  //如果串口接收到数据，就输出到蓝牙串口
+  //如果串口接收到数据，就输出到屏幕
   if (Serial.available()) {
     val = Serial.read();
-    BT.print(val);
-  }
 
-  //如果接收到蓝牙模块的数据，输出到屏幕
-  if (BT.available()) {
-    val = BT.read();
     Serial.print(val);
   }
 }
