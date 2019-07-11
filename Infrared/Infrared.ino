9d54e5019502220d7b54ea7a2853b0adccd49c82
Jaden Neal <longweicai1997@163.com>
2019-07-11 15:29:44 +0800

    infrared module

---
+++ Infrared/Infrared.ino
@@ -0,0 +1,18 @@
+// 以下为数字引脚接法
+#define infraOut 5  // 输出引脚号为5
+int infra = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(infraOut, INPUT);  // 红外传感器
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  infra = digitalRead(infraOut);
+  if (infra == 0)
+  {
+    Serial.println("Accept infra data!");
+  }
+
+}
